684269856dedd2c3b4569112e4cc7ddec16cfc65
Ahmed Abrar Ariyan <ahmedabrarariyan@gmail.com>
2024-02-10 21:56:52 +0600

    Created Lesson01

---
+++ L01: Variables.py
@@ -0,0 +1,28 @@
+# Lesson 1: Variables
+
+  # [variables er duita part ache. baam er ta 'key', aar daan er ta 'value']
+  # [assignment operator = diye variable er key te ekta value assign kora hoy]
+
+  #Example:
+name = "Abrar"
+age = 16
+  # [ekhane name aar age holo key. "Abrar" aar 16 holo value]
+
+  # [variables kokhono number diye shuru kora jabe na]
+    #i.e: 1name = "Abrar" --- eta invalid
+
+  # [variables er naam er majhe space deya jabe na]
+    #i.e: first name = "Abrar" --- eta invalid
+
+  # [variables e choto aar boro letters matter kore]
+    #i.e: Name = "Abrar" - this is different from name
+
+  # [kono 'Python keyword' ke variable hishebe declare kora jabe na]
+    #i.e: if, for, while etc
+    #key gula mukhosto kora lagbe na, code editor nijei alert dibe emon hole
+
+  # [variable er key gulate udvot characters use kora jabe na]
+    #i.e: name!, age% --- egula invalid
+
+  # [variables er key gulay space er bodole chaile _(underscore) use kora jay]
+    #i.e: first_name = "Abrar"
