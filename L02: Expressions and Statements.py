787b05b4af1fc87019612c358d1d581bc568cc86
Ahmed Abrar Ariyan <ahmedabrarariyan@gmail.com>
2024-02-11 22:30:31 +0600

    Created Lesson02, Lesson03

---
+++ L02: Expressions and Statements.py
@@ -0,0 +1,26 @@
+# Lesson 2: Expressions and Statements
+
+    #Expressions:
+    #[kono dhoroner code ja kono ekta value return kore]
+    #[othoba bola jay, eta kichu variable aar operator er shomonnoy]
+    #Examples:
+1 + 1   #--- eta ekta expression, jar value 2 
+"Abrar" #--- eta ekta expression, jar value ekta string
+
+    #Statements:
+    #[statement holo kono value er upor kora operation]
+    #Examples:
+name = "Abrar"
+age = 16
+print(name)
+print(name); print(age)
+        #uporer shobgulai ekekta statement
+        #name = "Abrar" e name variable e "Abrar" string assign korar operation ghoteche
+        #age = 16 e age variable e 16 int assign korar operation ghoteche
+        #print er statement gulay ekekta jinish print korar operation ghoteche
+
+    #[statement houar jonne oi line tar, value ke kichu ekta korte parar ability thakte hobe]
+ 
+    #Program: ekadhik statement er shomonnoye kono ekta program toiri hoy
+    #[amra ekek statement ekek line e likhte pari]
+    #[abar ek e line e semicolon diye ekadhik statement likha jay]
---
+++ L03: Comments.py
@@ -0,0 +1,16 @@
+#Lesson 3: Comments
+
+    #Python e, #(hash) er pore shob kichu main code theke ignore kora hoy
+    #orthat, code run korle # er pore likha jekono kichui code hishebe gonno hobe na
+    #Eta kei 'comment' bole
+
+    #Shadharonoto kono ekta line er jonne bishesh nirdeshona deyar jonne
+    #onno kauke othoba vobisshot e nijei abar mone korte parar jonne
+    #coder ra comment bebohar kore thaken
+
+    #Example:
+gaan = ("Subha Hone Na De", "Halka Halka Suroor Hai")
+    #Ami ei lesson ta likhar shomoy ei gaan gula shuntesilam
+print(gaan)
+
+    #Ekhane ami vobisshot e nijeke mone koranor jonne ekta note likhlam
